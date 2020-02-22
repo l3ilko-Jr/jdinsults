19680f58b638b0d6c882c40735f52c93f83fb6f2
l3ilko Jr <54482782+l3ilko-Jr@users.noreply.github.com>
2020-02-22 00:58:25 -0500

    Create insults.js

---
+++ Api/insults.js
@@ -0,0 +1,16 @@
+export let insults = [
+    "they said it isn't magic, its science. well, that doesnt seem to be true after the way you turned out",
+    "oh, you said you had a girlfriend? hold on, let me try to find it in your imagination",
+    "phenotype is the type of gene that you get for your looks. did the genes forget to assign you a few?",
+    "i wouldn't blame your parents for putting you up for adoption with a face like yours",
+    "oh, im sorry that your one brain cell couldn't comprehend my words. let me dumb it down for you",
+    "did you know theres something called a condom? you should use it to avoid making the same mistake your dad did",
+    "you have something on your chin... no, the 3rd one down.",
+    "You're the reason the gene pool needs a lifeguard.",
+    "If i had a face like yours, I'd sue my parents.",
+    "Your only chance of getting laid is to crawl up a chicken's butt and wait.",
+    "Some day you'll go far... and I hope you stay there.",
+    "Aha! I see the fuck-up fairy has visited us again!",
+    "You must have been born on a highway cos' that's where most accidents happen.",
+    "Imagine being 12"
+]
