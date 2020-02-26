3cab541a2fa2944849e53dc77c287023ef3ab6a8
l3ilko Jr <54482782+l3ilko-Jr@users.noreply.github.com>
2020-02-26 09:11:10 -0500

    Update jdinsults.js

--- jdinsults.js
+++ jdinsults.js
@@ -1,7 +1,7 @@
 const { insults } = require("./Api/insults");
 
-module.exports = function insults() {
-    const insult = insults[Math.floor(Math.floor() * insults.length)];
-
+Insults = () => {
+    const insult = insults[Math.floor(Math.random() * insults.length)];
+                           
     return insult;
 }
